10326d50f746a7740745e4be55475b0b66cb8034
DESKTOP-IGT9E3N\User <onlymouthman@gmail.com>
2024-08-19 21:15:28 +0800

    Test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 //const ASSET_URL = process.env.ASSET_URL || '';
-const ASSET_URL = (process.env.ASSET_URL || '') + '/MapWeb';
+const ASSET_URL = (process.env.ASSET_URL || '') + '';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
